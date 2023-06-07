66ebbeb0a378184b9b65dfbb12a91796cf260bf8
Zell Liew <zellwk@gmail.com>
2023-06-07 03:17:52 -0700

    Fix eslint errors

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,7 @@
-import { type SubtaskType } from '@/zustand'
+import { type SubtaskType } from "./types";
 
 export const getCompletedSubtasks = (subtasks: SubtaskType[]) => {
   return subtasks.reduce((prevValue: number, newSubtask) => {
-    return newSubtask.isCompleted ? prevValue + 1 : prevValue
-  }, 0)
-}
+    return newSubtask.isCompleted ? prevValue + 1 : prevValue;
+  }, 0);
+};
