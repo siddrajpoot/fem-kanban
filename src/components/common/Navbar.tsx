8730e0d994a9901c8ea0f6aab5708ece32e5a9eb
Sidd Rajpoot <siddrajpoot@gmail.com>
2023-06-12 23:02:45 -0700

    Add auth

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -2,6 +2,7 @@ import styles from "@/styles/Navbar.module.scss";
 import EllipsisIcon from "/public/icon-vertical-ellipsis.svg";
 import { useBoardStore } from "@/zustand/board";
 import { useModalStore } from "@/zustand/modal";
+import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 const Navbar = () => {
   const { setModalType } = useModalStore();
@@ -9,13 +10,18 @@ const Navbar = () => {
     (state) => state.boards[state.selectedBoardIndex]?.name
   );
 
+  const user = useUser();
+
   const handleClick = () => {
     setModalType("ADD_TASK");
   };
 
   return (
     <nav className={styles.navbar}>
-      <h3 className={styles.title}>{currentBoardName}</h3>
+      <h3 className={styles.title}>
+        <div>{currentBoardName}</div>
+        {user.isSignedIn ? <SignOutButton /> : <SignInButton />}
+      </h3>
       <button className={styles.addNew} onClick={handleClick}>
         + Add New Task
       </button>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { type Session } from 'next-auth'
-import { SessionProvider } from 'next-auth/react'
-import { type AppType } from 'next/app'
-import { api } from '@/utils/api'
-import Modal from '@/components/common/Modal'
+import { type Session } from "next-auth";
+import { type AppType } from "next/app";
+import { api } from "@/utils/api";
+import Modal from "@/components/common/Modal";
 
-import '@/styles/reset.css'
-import '@/styles/globals.scss'
-import Layout from '@/components/common/layout'
+import "@/styles/reset.css";
+import "@/styles/globals.scss";
+import Layout from "@/components/common/layout";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps,
 }) => {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider>
       <Layout>
         <Component {...pageProps} />
         <Modal />
       </Layout>
-    </SessionProvider>
-  )
-}
+    </ClerkProvider>
+  );
+};
 
-export default api.withTRPC(MyApp)
+export default api.withTRPC(MyApp);
